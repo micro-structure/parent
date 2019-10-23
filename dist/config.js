d608899c67ba4e5a3eced82b72782d5cb4f62649
yangming <472102644@qq.com>
2019-10-23 15:03:25 +0800

    fix: build app

---
+++ dist/config.js
@@ -0,0 +1,48 @@
+window._MICRO_APP_CONFIG_SERVER = 'http://yourdomain.com'
+window._MICRO_APP_CONFIG_MENU = [
+  {
+    id: 'index',
+    name: '首页',
+    path: '/',
+    dir: '',
+    entry: '',
+    child: [
+      {
+        name: '个人中心',
+        path: '/user',
+        child: [
+          {
+            name: '密码设置',
+            path: '/user/pwd'
+          }
+        ]
+      }
+    ]
+  },
+  {
+    id: 'second',
+    name: '第二个子项目',
+    path: '/second',
+    dir: 'second',
+    child: [
+      {
+        name: '我的订单',
+        path: '/second/order',
+        child: [
+          {
+            name: '最近订单',
+            path: '/second/order/latest'
+          },
+          {
+            name: '退款售后',
+            path: '/second/order/back'
+          }
+        ]
+      },
+      {
+        name: '我的积分',
+        path: '/second/jifen'
+      }
+    ]
+  }
+]
